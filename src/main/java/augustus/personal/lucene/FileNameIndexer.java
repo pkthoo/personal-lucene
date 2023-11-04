9919db4993dbd189c413ecafb8080f97a5ed76a1
pk_th <pk_thoo@yahoo.com>
2023-11-04 09:44:35 +0800

    [3.0.0] FileNameIndexer - using virtual threads to speed up indexing

--- src/main/java/augustus/personal/lucene/FileNameIndexer.java
+++ src/main/java/augustus/personal/lucene/FileNameIndexer.java
@@ -7,31 +7,45 @@ import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.store.FSDirectory;
 
+import java.io.Closeable;
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Paths;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
 
 /**
  * Created by thooaug on 24/8/2023.
+ *
+ * BENCHMARK:
+ *  - [2023-11-04]
+ *    - Java 17 -- 33 seconds (baseline)
+ *    - Java 21 -- 18 seconds
  */
 @Slf4j
-public class FileNameIndexer {
+public class FileNameIndexer implements Closeable {
 
     public static void main(String[] args) throws Exception {
+        Path path = Path.of("lucene");
+        DeleteAll.doDeleteAll(path);
         log.info("START INDEXING NOW");
-        FileNameIndexer indexer = new FileNameIndexer("lucene");
-        try {
+        long elapsed = System.currentTimeMillis();
+        try (FileNameIndexer indexer = new FileNameIndexer(path)) {
             File dir = new File(args[0]);
             indexer.indexFiles(dir);
-        } finally {
-            indexer.close();
         }
-        log.info("DONE INDEXING");
+        elapsed = System.currentTimeMillis() - elapsed;
+        elapsed = elapsed / 1000;
+        log.info("DONE INDEXING AFTER {} seconds", elapsed);
     }
 
     int limit = 2097152;
     final StandardAnalyzer analyzer;
     final FSDirectory indexDirectory;
+    final boolean TRACE = false;
+    final ExecutorService pool;
+    final List<Future<?>> tasks;
 
     String[] indexAllowedExts = new String[]{
             ".java", ".xml", ".yml", ".properties"
@@ -43,21 +57,84 @@ public class FileNameIndexer {
 
     String indexAllowedRegex = null;
 
-    public FileNameIndexer(String indexPath) throws IOException {
+    public FileNameIndexer(Path path) throws IOException {
         analyzer = new StandardAnalyzer();
-        indexDirectory = FSDirectory.open(Paths.get(indexPath));
+        indexDirectory = FSDirectory.open(path);
+        pool = Executors.newVirtualThreadPerTaskExecutor();
+        tasks = new ArrayList<>(100);
     }
 
+    @Override
     public void close() throws IOException {
+        closePool();
         analyzer.close();
         indexDirectory.close();
+    }
+
+    private void awaitTasksCompletion() {
+        int completedCount = 0;
+        try {
+            Future<?> task;
+            //do {
+                while (!tasks.isEmpty()) {
+                    synchronized (tasks) {
+                        task = tasks.remove(0);
+                    }
+                    if (task.isDone()) {
+                        ++completedCount;
+                        continue;
+                    }
+                    try {
+                        task.get(100, TimeUnit.MILLISECONDS);
+                        ++completedCount;
+                    } catch (TimeoutException e) {
+                        // reinsert to retry later
+                        synchronized (tasks) {
+                            tasks.add(task);
+                        }
+                    }
+                }
+            //    Thread.sleep(100);
+            //    log.debug("... {} TASKS REMAINING", tasks.size());
+            //} while (!tasks.isEmpty());
+            Thread.sleep(100);
+        } catch (Exception e) {
+            log.error("", e);
+        }
+        log.debug("{} TASKS COMPLETED, {} REMAINING", completedCount, tasks.size());
+    }
+
+    private void closePool() {
+        pool.shutdown();
+        //try {
+        //    if (!pool.awaitTermination(10, TimeUnit.SECONDS)) {
+        //        pool.shutdownNow();
+        //    }
+        //} catch (InterruptedException e) {
+        //    log.error(e.toString());
+        //}
+        log.debug("VIRTUAL THREAD POOL SHUTDOWN");
     }
 
     public void indexFiles(File dir) throws IOException {
         IndexWriterConfig config = new IndexWriterConfig(analyzer);
         try (IndexWriter indexWriter = new IndexWriter(indexDirectory, config)) {
-            indexFilesRecursively(indexWriter, dir);
+            asyncIndexFilesRecursively(indexWriter, dir);
+            awaitTasksCompletion();
             indexWriter.commit();
+        }
+    }
+
+    private void asyncIndexFilesRecursively(IndexWriter indexWriter, File file) {
+        Future<Object> task = pool.submit(new Callable<Object>() {
+            @Override
+            public Object call() throws Exception {
+                FileNameIndexer.this.indexFilesRecursively(indexWriter, file);
+                return 0;
+            }
+        });
+        synchronized (tasks) {
+            tasks.add(task);
         }
     }
 
@@ -65,11 +142,11 @@ public class FileNameIndexer {
         FileForIndex source = FileForIndex.from(file);
         if (!indexAllowed(source)) return;
         if (file.isDirectory()) {
-            log.debug("[INDEX_DIR ] -- {}", file.getAbsolutePath());
+            if (TRACE) log.trace("[INDEX_DIR ] -- {}", file.getAbsolutePath());
             File[] files = file.listFiles();
             if (files != null) {
                 for (File subFile : files) {
-                    indexFilesRecursively(indexWriter, subFile);
+                    asyncIndexFilesRecursively(indexWriter, subFile);
                 }
             }
         } else {
@@ -88,7 +165,7 @@ public class FileNameIndexer {
         }*/
         Document document = source.asDocument(limit);
         if (document != null) {
-            log.debug("[INDEX_FILE] -- {}", source);
+            if (TRACE) log.trace("[INDEX_FILE] -- {}", source);
             indexWriter.addDocument(document);
         }
     }
@@ -109,7 +186,7 @@ public class FileNameIndexer {
             if (excludeDirContains != null) {
                 for (String token : excludeDirContains) {
                     if (source.path().contains(token)) {
-                        log.info("[SKIP] -- {}", source.path());
+                        if (TRACE) log.trace("[SKIP] -- {}", source.path());
                         return false;
                     }
                 }
